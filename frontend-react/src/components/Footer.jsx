a66f4dd9ae17581e96304314aa68f243df05ffd5
Garg <aditya.garg@innovasolutions.com>
2025-11-12 11:25:20 +0530

    home page

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='footer py-3 my-3'>
+        <hr className='border-bottom'/>
+        <p className="text-light text-center">&copy; 2025 - Built with love 😊 by Aditya</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className="container ">
+            <div className="p-5 text-center bg-light-dark rounded">
+                <h1 className="text-light">Stock Prediction Portal</h1>
+                <p className='text-light lead'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia magnam adipisci ad asperiores qui, odio, deserunt possimus perspiciatis accusamus aliquam nihil architecto doloremque vitae a perferendis. Alias inventore odit earum.</p>
+                <Button text='Login Now' class= 'btn-info'/>
+            </div>
+        </div>
+    </>
+  )}
+
+export default Main
