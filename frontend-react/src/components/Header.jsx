f3d33efc93cca919dc48aeae0987c6c6421e22aa
Garg <aditya.garg@innovasolutions.com>
2025-11-17 11:53:30 +0530

    Login function

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from './Button'
 import { Link, Links } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <>
-    <nav className='navbar container pt-3 pb-3 align-items-start'>
-        <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
-       
-        <div>
-            <Button text='Login' class = 'btn-outline-info' url='/login'/>
-            &nbsp;
-            <Button text='Register' class= 'btn-info' url='/register'/>
-        </div>
+    const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const handleLogout = () => {
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+        setIsLoggedIn(false)
+        navigate('/login')
+    }
+    return ( 
+        <>
+            <nav className='navbar container pt-3 pb-3 align-items-start'>
+                <Link className='navbar-brand text-light' to="/">Stock Prediction Portal</Link>
 
-    </nav>
-    </>
-  )
+                <div>
+                    {isLoggedIn ? (
+                        <button className='btn btn-danger' onClick={handleLogout} >Logout</button>
+                    ) : (
+                        <>
+                            <Button text='Login' class='btn-outline-info' url='/login' />
+                            &nbsp;
+                            <Button text='Register' class='btn-info' url='/register' />
+                        </>
+                    )}
+
+                </div>
+
+            </nav>
+        </>
+    )
 }
 
 export default Header
